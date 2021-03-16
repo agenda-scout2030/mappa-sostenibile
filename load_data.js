2606c9a7a57f35dd1fbd30dcce9ff5dc4c4d96da
Tommimon <37435103+Tommimon@users.noreply.github.com>
2021-03-16 16:11:51 +0100

    Initial commit

---
+++ load_data.js
@@ -0,0 +1,23 @@
+var url="https://spreadsheets.google.com/feeds/cells/1SbRQO-gG28PeCcYJ1uu87_jMw3oDEBh1sx6tAMAVFMc/1/public/full?alt=json";
+
+var jsonData; // save data for next script
+
+xmlhttp=new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if(xmlhttp.readyState === 4 && xmlhttp.status===200){
+        jsonData = xmlhttp.responseText;
+        create();  // we have the data so we can create the map
+    }
+};
+xmlhttp.open("GET",url,true);
+xmlhttp.send(null);
+
+// add to the document the script that creates the map
+function create() {
+    var newScript = document.createElement('script');
+    newScript.setAttribute('type', 'text/javascript');
+    newScript.setAttribute('src', 'https://www.bing.com/api/maps/mapcontrol?callback=GetMap&key=AvBiDn5MrB3LBYSYeO65dHXFuOO69QAw82FEXcQh6N5x-QwKdN1plg6kPxpmwV1a');
+    newScript.setAttribute('async', "");  // means async true
+    newScript.setAttribute('defer', "");  // means defer true
+    document.body.appendChild(newScript);
+}
---
+++ map.js
@@ -0,0 +1,128 @@
+var map;
+var pinList = [];  // create an object
+
+var infobox;
+
+const icon = "https://raw.githubusercontent.com/agenda-scout2030/mappa-sostenibile/master/assets/giglio.png";
+
+// called from bing api
+function GetMap() {
+    parseMarkers();  // fill pins list
+
+    map = new Microsoft.Maps.Map('#myMap', {
+        center: new Microsoft.Maps.Location(44.643, 10.925229),
+        zoom: 17,
+    });
+
+    // initialize infobox for future use
+    infobox = new Microsoft.Maps.Infobox(map.getCenter(), {
+        offset: new Microsoft.Maps.Point(0, 50),  // the amount the info box bottom is shifted from the pin location
+        visible: false,  // not visible in the beginning
+        maxWidth: 350,
+    });
+    infobox.setMap(map);
+
+    //  clickable layer for the pins
+    var layer = new Microsoft.Maps.Layer();
+
+    for(let i = 0; i < pinList.length; i++) {
+        let pinInfo = pinList[i];
+
+        if (pinInfo.approved === '1') {  // check moderator cell value
+            // create pushpin
+            var pin = new Microsoft.Maps.Pushpin(
+                new Microsoft.Maps.Location(pinInfo.location.lat, pinInfo.location.lng), {
+                    title: pinInfo.name,
+                    icon: icon,
+                }
+            );
+
+            pin.metadata = pinInfo;
+
+            // add the pushpin to the layer
+            layer.add(pin);
+        }
+    }
+
+    // add a click handler to the layer.
+    Microsoft.Maps.Events.addHandler(layer, 'click', displayInfobox);
+
+    map.layers.insert(layer);
+
+    // once map initialization is finished show the add button
+    document.getElementById('addBtn').removeAttribute('hidden');
+}
+
+// parse markers from row input json
+function parseMarkers() {
+    let text;
+    text = jsonData;
+    if (text !== "") {
+        let entries = JSON.parse(text).feed.entry;
+        for(let i = 0; i < entries.length; i++) {
+            let cell = entries[i]['gs$cell'];
+            let row = cell.row - 2;  // spreadsheets counts from 1 and first row contains labels
+            let col = cell.col;
+            let val = cell['$t'];
+            if (row >= 0) {
+                if (pinList[row] === undefined) {
+                    pinList[row] = {  // initialize with default values
+                        name: "",
+                        title: "",
+                        description: "",
+                        location: {},
+                    };
+                }
+                switch (col) {
+                    case '2':
+                        pinList[row].name = val;
+                        break;
+                    case '7':
+                        pinList[row].title = val;
+                        break;
+                    case '3':
+                        pinList[row].description = val;
+                        break;
+                    case '4':
+                        pinList[row].location.lat = val;
+                        break;
+                    case '5':
+                        pinList[row].location.lng = val;
+                        break;
+                    case '6':
+                        pinList[row].link = val;
+                        break;
+                    case '8':
+                        pinList[row].image = val;
+                        break;
+                    case '9':
+                        pinList[row].approved = val;
+                        break;
+                }
+            }
+        }
+    }
+}
+
+
+function displayInfobox(event) {
+    let pin = event.target;
+
+    let body = [];  // info box body
+
+    body.push('<div class="infowindow-content">');
+    body.push('<p>' + pin.metadata.description);
+    // TODO: add image
+    body.push('</p>');
+    if (!(pin.metadata.link === undefined)) {  // if link is defined
+        body.push('<div class="bottomtext"><a href="' + pin.metadata.link + '" target=_blank>Scopri di più...</a></div>');
+    }
+    body.push('</div>');
+
+    infobox.setOptions({
+        location: pin.getLocation(),  // move the infobox to the new location
+        title: pin.metadata.title,
+        description: body.join(''),  // accepts a string as html to set the infobox body
+        visible: true  // show infobox if is hidden
+    })
+}

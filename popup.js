15c8764450eacf46d5aa67253d0aa4d4f6ce45a8
Tommimon <37435103+Tommimon@users.noreply.github.com>
2021-03-20 16:49:09 +0100

    Added popup info box

---
+++ popup.js
@@ -0,0 +1,27 @@
+$(window).load(function () {
+    const popup = $('.popupBackground')
+
+    // show on startup
+    popup.show()
+
+    // open popup when info image is clicked
+    $(".infoButton").click(function(){
+        $('.popupBackground').show();
+    });
+
+    // hide popup when user click outside
+    popup.click(function(e){
+        const container = $('.innerPopupBox');
+
+        // if the target of the click isn't the container nor a descendant of the container
+        if (!container.is(e.target) && container.has(e.target).length === 0)
+        {
+            $('.popupBackground').hide();
+        }
+    });
+
+    // hide popup when close button is clocked
+    $('.popupCloseButton').click(function(){
+        $('.popupBackground').hide();
+    });
+});

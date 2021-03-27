db86b499646c6cab95307de3cb37acfbbd422980
Tommimon <37435103+Tommimon@users.noreply.github.com>
2021-03-27 22:38:50 +0100

    Added geolocate function

--- geolocator.js
+++ geolocator.js
@@ -1,17 +1,20 @@
-const data = {
-    addressdetails: 1,
-    city: 'Milano',
-    country: 'Italia',
-    state: 'Lombardia',
-    street: 'Via Monte Napoleone, 15',
-    format: 'json',
-};
+function geolocate() {
+    alert("Grazie per aver compilato il form");
+    const data = {
+        addressdetails: 1,
+        city: document.getElementById("citta").value,
+        country: 'Italia',
+        state: document.getElementById("regione").value,
+        street: document.getElementById("strada").value,
+        format: 'json',
+    };
 
-$.ajax({
-    type: "GET",
-    url: 'https://nominatim.openstreetmap.org/search',
-    data: data,
-    success: function (data) {
-        console.log(data);
-    },
-});
+    $.ajax({
+        type: "GET",
+        url: 'https://nominatim.openstreetmap.org/search',
+        data: data,
+        success: function (data) {
+            console.log(data);
+        },
+    });
+}

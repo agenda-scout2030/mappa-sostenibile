ad5cb723598dc5e3b551126450431a9ac177d045
Tommimon <37435103+Tommimon@users.noreply.github.com>
2021-03-27 23:09:47 +0100

    Setting the geolocator response as options in the selection

--- geolocator.js
+++ geolocator.js
@@ -14,6 +14,16 @@ function geolocate() {
         data: data,
         success: function (data) {
             console.log(data);
+            let menu = document.getElementById("scelta");
+            for (let j = 0; menu.children.length > 0; j++) {
+                menu.removeChild(menu.children[0]);
+            }
+            for (let i = 0; i < data.length; i++) {
+                let newOption = document.createElement('option');
+                newOption.setAttribute("value", i.toString());
+                newOption.innerHTML = data[i]["display_name"];
+                menu.appendChild(newOption);
+            }
         },
     });
 }
